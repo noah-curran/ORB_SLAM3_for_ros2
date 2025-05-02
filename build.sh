8e1929a838309ffdaca75cdb22ea6834e60ed8af
Noah Curran <swimnoah113@gmail.com>
2025-05-02 14:32:02 -0400

    update to c++17

--- build.sh
+++ build.sh
@@ -37,4 +37,4 @@ echo "Configuring and building ORB_SLAM3 ..."
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j4
+make -j4 CXXFLAGS+="-Wno-unused-variable -Wno-class-memaccess -Wno-reorder -Wno-deprecated-declarations"
